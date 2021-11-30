2da991f1b5d442d07c076c644ceff04f2571d286
eduardozuppodev <zuppo.edu@gmail.com>
2021-11-30 09:24:37 -0300

    feat: routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { GlobalStyles } from '../themes';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Home, Abstract } from '../pages';
+import { Routing } from './routing';
+import { inject, observer } from 'mobx-react';
+
+const RoutesContainer = () => (
+  <>
+    <GlobalStyles />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={Routing.HOME} component={Home} />
+        <Route exact path={Routing.ABSTRACT} component={Abstract} />
+      </Switch>
+    </BrowserRouter>
+  </>
+);
+
+export { Routing };
+export default inject('routing')(observer(RoutesContainer));
---
+++ src/routes/routing.tsx
@@ -0,0 +1,4 @@
+export enum Routing {
+  HOME = '/',
+  ABSTRACT = '/resumo'
+}
