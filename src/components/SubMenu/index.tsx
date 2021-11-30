b88235b49fc590e40377a9dfe7130a8a3738401f
eduardozuppodev <zuppo.edu@gmail.com>
2021-11-30 12:25:27 -0300

    feat: add condition

--- src/components/SubMenu/index.tsx
+++ src/components/SubMenu/index.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import { Container, Option } from './styles';
 
 export const SubMenu: React.FC = () => {
-  return <div />;
+  const [subActive, setSubActive] = useState('PLACEBO&LARANJA');
+
+  return (
+    <Container>
+      <Option
+        onClick={() => setSubActive('PLACEBO&LARANJA')}
+        subActive={subActive === 'PLACEBO&LARANJA'}
+      >
+        PLACEBO VS. LARANJA
+      </Option>
+      <Option
+        onClick={() => setSubActive('LARANJA')}
+        subActive={subActive === 'LARANJA'}
+      >
+        LARANJA
+      </Option>
+      <Option
+        onClick={() => setSubActive('TANGERINA')}
+        subActive={subActive === 'TANGERINA'}
+      >
+        TANGERINA
+      </Option>
+    </Container>
+  );
 };
--- src/components/SubMenu/styles.ts
+++ src/components/SubMenu/styles.ts
@@ -1,3 +1,29 @@
 import styled from 'styled-components';
+import { colors } from 'themes';
 
-export const Container = styled.div``;
+type Props = {
+  subActive?: boolean;
+};
+
+export const Container = styled.div`
+  width: 100%;
+  height: 7%;
+  background-color: #ffcd9b58;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const Option = styled.button`
+  height: 100%;
+  width: 25%;
+  border-top: ${(props: Props) =>
+    props.subActive ? '1px solid #fe7500' : 'none'};
+
+  color: ${colors.main.background};
+  font-weight: ${(props: Props) => (props.subActive ? '700' : '300')};
+  font-size: 13px;
+  cursor: pointer;
+  margin: 0 10px;
+`;
