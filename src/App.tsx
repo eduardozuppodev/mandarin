7953dea56413b0a187b54716e98736af18309584
eduardozuppodev <zuppo.edu@gmail.com>
2021-11-30 09:25:01 -0300

    fix: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import Routes from './routes';
+import { Router } from 'react-router-dom';
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+import store from './stores';
 
-const App = () => <Routes />;
+import { createBrowserHistory } from 'history';
+import { Provider } from 'mobx-react';
+
+const browserHistory = createBrowserHistory();
+const routingStore = new RouterStore();
+const routing = syncHistoryWithStore(browserHistory, routingStore);
+
+const stores = { ...store, routing };
+
+const App = () => (
+  <Provider {...stores}>
+    <Router history={routing}>
+      <Routes />
+    </Router>
+  </Provider>
+);
 
 export default App;
