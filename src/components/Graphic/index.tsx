dd7ff4698eeef72662f52b72e3e3a2deab1e9a8d
eduardozuppodev <zuppo.edu@gmail.com>
2021-12-01 12:05:15 -0300

    feat: select

--- src/components/Graphic/index.tsx
+++ src/components/Graphic/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Header, Title, Span, Select, Option } from './styles';
 
 export const Graphic: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Header>
+        <Title>Mandarin</Title>
+        <Span>vs.</Span>
+        <Select>
+          <Option>Placebo vs. Laranja</Option>
+          <Option>Laranja</Option>
+          <Option>Tangerina</Option>
+        </Select>
+      </Header>
+    </Container>
+  );
 };
--- src/components/Graphic/styles.ts
+++ src/components/Graphic/styles.ts
@@ -6,3 +6,42 @@ export const Container = styled.div`
   border: 2px solid #ffeedc;
   border-radius: 4px;
 `;
+
+export const Header = styled.div`
+  width: 100%;
+  height: 80px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const Title = styled.h1`
+  color: #fe7500;
+  font-weight: 500;
+  font-size: 24px;
+`;
+
+export const Span = styled.span`
+  margin: 0 10px;
+  color: #666666;
+  font-weight: 100;
+  font-size: 24px;
+`;
+
+export const Select = styled.select`
+  border: 0;
+  width: 200px;
+  height: 50%;
+  position: relative;
+  float: left;
+  font-family: 'Roboto';
+  font-weight: 100;
+  font-size: 18px;
+  color: #666666;
+  background: #ffeedc;
+  border: none;
+`;
+
+export const Option = styled.option``;
